44b28fe8ce31e6985206b7c82cdb95e76233d1c7
Egor4ik325 <zorinegor352@gmail.com>
2021-07-13 22:17:37 +0300

    Improve algorithm

--- huffman_coding_algorithm/encode.py
+++ huffman_coding_algorithm/encode.py
@@ -10,16 +10,11 @@ def huffman_encode(s):
     :param s: byte string to encode
     :returns: encoded string
     """
-    # Set of all unique characters
-    chars = set()
-    for c in s:
-        chars.add(c)
-    chas = list(chars)
+    # Unique sorted list (avoid randomness)
+    chars = sorted(list(set([c for c in s])))
 
     # Character frequency hash table {chars: freq}
-    freq = {}
-    for c in chars:
-        freq[c] = s.count(c)
+    freq = dict([(c, s.count(c)) for c in chars])
 
     # Huffman tree hash table {node: node}
     tree = {}
@@ -47,15 +42,8 @@ def huffman_encode(s):
         non_processed.update([new_node])
 
         # Node sides {chars: 0|1}
-        if left_node[1] == right_node[1]:
-            sides[left_node[0]] = 0
-            sides[right_node[0]] = 1
-        elif left_node[1] < right_node[1]:
-            sides[left_node[0]] = 0
-            sides[right_node[0]] = 1
-        else:
-            sides[left_node[0]] = 1
-            sides[right_node[0]] = 0
+        sides[left_node[0]] = 0
+        sides[right_node[0]] = 1
 
         # Add nodes to tree hash table {node: node}
         tree[left_node[0]] = new_node[0]
@@ -64,7 +52,7 @@ def huffman_encode(s):
         # Root node
         tree[new_node[0]] = None
 
-    # Transform tree into huffman codes hash table
+    # Transform tree into huffman codes (binary tree)
     codes = {}
     for c in chars:
         node = c
@@ -73,13 +61,14 @@ def huffman_encode(s):
 
         # Go throught the tree to the root
         while tree[node2]:
-            codes[c] += str(sides[node2])
+            # Prepend character from the start (e.g. 1010 - right, left, right, left)
+            codes[c] = str(sides[node2]) + codes[c]
             node2 = tree[node2]
 
-    # Binary string
+    # Binary coding string
     s2 = ''.join([codes[c] for c in s])
 
-    return s2, codes
+    return s2, codes, chars
     # print(
     #     f'Initial: "{s}", {len(s)*8} bit',
     #     f'Final: {s2}, {len(s2)} bit',
@@ -87,50 +76,44 @@ def huffman_encode(s):
     # )
 
 
-if __name__ == '__main__':
-    # String for encoding
-    s = "Hello, World!"
-
-    # Encode string using Huffman coding (0.6s, 10000 times)
-    coding, codes = huffman_encode(s)
-    print(coding)
-
+def encode_file(s):
+    """Compress given string and write to the file."""
+    # Convert string to binary string
+    coding, _, chars = huffman_encode(s)
     # Convert binary string to bytes
-    coding = int(coding, 2).to_bytes(
+    coding_number = int(coding, 2)
+    coding_bytes = coding_number.to_bytes(
         (len(coding) + 7) // 8, byteorder=byteorder)
-    print(coding)
 
-    # Convert bit string to packed binary data
-    # binary_data = pack('H', coding)
-
-    # Write output to the file
+    # Write binary bytes
     with open('out.txt', 'wb') as f:
-        f.write(coding)
+        f.write(coding_bytes)
 
-    # Write huffman codes to the file
+    # Max huffman tree depth (max codes length)
+    # max_tree_depth = max(map(len, codes.values()))
+
+    # Write codes into file (how?)
+
+    # Write all unique characters in the string
+    # to regenerate codes based on the same code
+    # with open('chars.txt', 'w') as f:
+    #     unique_s = ''.join([c for c in chars])
+    #     f.write(unique_s)
+
+
+def decode_file():
+    """Decompresses/decodes huffman coding file with unique chars."""
+    # with open('chars.txt') as f:
+    #     unique_s = f.read()
 
     # Read encoded data
     with open('out.txt', 'rb') as f:
         encoded_data = f.read()
 
-    print(encoded_data)
-
-    # def bytes2binstr(b, n=None, k=8):
-    #     s = '{x:0{m}b}'.format(
-    #         m=len(b) * 8, x=int.from_bytes(b, byteorder=byteorder))[:n]
-    #     return s
-
-    # encoded_s2 = bytes2binstr(encoded_data)
     encoded_s2 = int.from_bytes(encoded_data, byteorder=byteorder)
-    print(bin(encoded_s2))
-
     encoded_s2 = bin(encoded_s2)
     encoded_s2 = encoded_s2.lstrip('0b')
-
     inv_codes = {v: k for k, v in codes.items()}
-
-    print(codes)
-    print(inv_codes)
 
     msg = ''
     bits = ''
@@ -142,4 +125,18 @@ if __name__ == '__main__':
             msg += c
             bits = ''
 
-    print(msg)
+
+if __name__ == '__main__':
+    # String for encoding
+    s = "Hello, World!"
+
+    s2, codes, chars = huffman_encode(s)
+    print('Coding bitstring:', s2)
+    print('Character codes:')
+    from pprint import pprint
+    pprint(codes)
+    print('Used characters:')
+    from pprint import pprint
+    pprint(chars)
+
+    encode_file(s)
