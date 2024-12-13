8e0242425c87c036a7d1c38aee0f7a4bd75755bc
nezort11 <nezort11@gmail.com>
2024-12-13 10:13:25 +0300

    Solve product except self problem in O(1) space

--- product-of-array-except-self/indes.ts
+++ product-of-array-except-self/indes.ts
@@ -4,29 +4,43 @@ function* range(start: number, stop: number, step = 1) {
   }
 }
 
-// algo O(2n), space O(2n + n)
+// algo O(2n), space O(2n + n) - 25ms
+// function productExceptSelf(nums: number[]): number[] {
+//   const productStart = {};
+//   const productEnd = {};
+//   for (const numsIndex of range(0, nums.length)) {
+//     const numStart = nums[numsIndex];
+//     const prevProd = productStart[numsIndex - 1] ?? 1;
+//     productStart[numsIndex] = prevProd * numStart;
+//     const numsEndIndex = nums.length - 1 - numsIndex;
+//     const numEnd = nums[numsEndIndex];
+//     const nextProd = productEnd[numsEndIndex + 1] ?? 1;
+//     productEnd[numsEndIndex] = nextProd * numEnd;
+//   }
+//   // console.log("productStart", productStart);
+//   // console.log("productEnd", productEnd);
+//   const productExcept = new Array(nums.length);
+//   for (const numsIndex of range(0, nums.length)) {
+//     const startProduct = productStart[numsIndex - 1] ?? 1;
+//     const endProduct = productEnd[numsIndex + 1] ?? 1;
+//     productExcept[numsIndex] = startProduct * endProduct;
+//   }
+//   return productExcept;
+// }
+
+// algo O(2n), space O(1 + n) - 25ms
 function productExceptSelf(nums: number[]): number[] {
-  const productStart = {};
-  const productEnd = {};
-  for (const numsIndex of range(0, nums.length)) {
-    const numStart = nums[numsIndex];
-    const prevProd = productStart[numsIndex - 1] ?? 1;
-    productStart[numsIndex] = prevProd * numStart;
-
-    const numsEndIndex = nums.length - 1 - numsIndex;
-    const numEnd = nums[numsEndIndex];
-    const nextProd = productEnd[numsEndIndex + 1] ?? 1;
-    productEnd[numsEndIndex] = nextProd * numEnd;
-  }
-
-  // console.log("productStart", productStart);
-  // console.log("productEnd", productEnd);
-
   const productExcept = new Array(nums.length);
   for (const numsIndex of range(0, nums.length)) {
-    const startProduct = productStart[numsIndex - 1] ?? 1;
-    const endProduct = productEnd[numsIndex + 1] ?? 1;
-    productExcept[numsIndex] = startProduct * endProduct;
+    const numStart = nums[numsIndex];
+    const prevProd = productExcept[numsIndex - 1] ?? 1;
+    productExcept[numsIndex] = prevProd * numStart;
+  }
+  let productFromEnd = 1;
+  for (const numsIndex of range(nums.length - 1, -1, -1)) {
+    const prevNum = productExcept[numsIndex - 1] ?? 1;
+    productExcept[numsIndex] = prevNum * productFromEnd;
+    productFromEnd *= nums[numsIndex];
   }
 
   return productExcept;
