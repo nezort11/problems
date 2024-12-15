9086c24aac648f742a96f4f51f9e8d354beb7df1
nezort11 <nezort11@gmail.com>
2024-12-15 03:10:53 +0300

    Add division without division to product without self problem

--- product-of-array-except-self/indes.ts
+++ product-of-array-except-self/indes.ts
@@ -29,18 +29,56 @@ function* range(start: number, stop: number, step = 1) {
 // }
 
 // algo O(2n), space O(1 + n) - 25ms
+// function productExceptSelf(nums: number[]): number[] {
+//   const productExcept = new Array(nums.length);
+//   for (const numsIndex of range(0, nums.length)) {
+//     const numStart = nums[numsIndex];
+//     const prevProd = productExcept[numsIndex - 1] ?? 1;
+//     productExcept[numsIndex] = prevProd * numStart;
+//   }
+//   let productFromEnd = 1;
+//   for (const numsIndex of range(nums.length - 1, -1, -1)) {
+//     const prevNum = productExcept[numsIndex - 1] ?? 1;
+//     productExcept[numsIndex] = prevNum * productFromEnd;
+//     productFromEnd *= nums[numsIndex];
+//   }
+
+//   return productExcept;
+// }
+
+function restoringDivision(dividend: number, divisor: number) {
+  let quotient = 0;
+  let remainder = 0;
+
+  // We use a 32-bit simulation (assume 32-bit integers for this example)
+  for (let i = 31; i >= 0; i--) {
+    // Shift remainder left by 1 and bring down the next bit of the dividend
+    remainder = (remainder << 1) | ((dividend >> i) & 1);
+
+    // Shift quotient left to make room for the next bit
+    quotient <<= 1;
+
+    // Subtract divisor if remainder is >= divisor
+    if (remainder >= divisor) {
+      remainder -= divisor;
+      quotient |= 1; // Set the least significant bit of the quotient
+    }
+  }
+
+  return quotient;
+}
+
+// O(n)
 function productExceptSelf(nums: number[]): number[] {
+  let product = 1;
+  for (const num of nums) {
+    product *= num;
+  }
+
   const productExcept = new Array(nums.length);
   for (const numsIndex of range(0, nums.length)) {
-    const numStart = nums[numsIndex];
-    const prevProd = productExcept[numsIndex - 1] ?? 1;
-    productExcept[numsIndex] = prevProd * numStart;
-  }
-  let productFromEnd = 1;
-  for (const numsIndex of range(nums.length - 1, -1, -1)) {
-    const prevNum = productExcept[numsIndex - 1] ?? 1;
-    productExcept[numsIndex] = prevNum * productFromEnd;
-    productFromEnd *= nums[numsIndex];
+    const num = nums[numsIndex];
+    productExcept[numsIndex] = restoringDivision(product, num);
   }
 
   return productExcept;
@@ -48,4 +86,4 @@ function productExceptSelf(nums: number[]): number[] {
 
 console.log(productExceptSelf([1, 2, 3, 4]));
 
-// console.log(productExceptSelf([-1, 1, 0, -3, 3]));
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
