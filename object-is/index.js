ba9f9622ccd61724b99c6b5c9cd5dd12e54bfb9b
nezort11 <nezort11@gmail.com>
2025-04-29 00:31:19 +0300

    Solve Object.is problem

---
+++ object-is/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {any} a
+ * @param {any} b
+ * @return {boolean}
+ */
+function is(a, b) {
+  if (Number.isNaN(a) && Number.isNaN(b)) {
+    return true;
+  }
+  if (a === 0 && b === 0) {
+    return 1 / a === 1 / b;
+  }
+  return a === b;
+}
