9a3f766277fe1eeb321b1725a5e4ee5bebc5543d
nezort11 <nezort11@gmail.com>
2025-05-15 00:31:27 +0300

    Solve missing number in the bestest way

--- find-missing-number-sequence/index.js
+++ find-missing-number-sequence/index.js
@@ -11,21 +11,29 @@
  * - [0, 3] => never
  * - [3] => never
  */
-export default function findMissingNumberInSequence(numbers) {
-  const numberSet = new Set();
-  // O(n)
-  for (const num of numbers) {
-    numberSet.add(num);
-  }
+// export default function findMissingNumberInSequence(numbers) {
+//   const numberSet = new Set();
+//   // O(n)
+//   for (const num of numbers) {
+//     numberSet.add(num);
+//   }
 
-  // O(n + 1) => O(n)
-  for (const n = 0; n <= numbers.length; n += 1) {
-    if (numberSet.has(n)) {
-      continue;
-    } else {
-      return n;
-    }
-  }
+//   // O(n + 1) => O(n)
+//   for (const n = 0; n <= numbers.length; n += 1) {
+//     if (numberSet.has(n)) {
+//       continue;
+//     } else {
+//       return n;
+//     }
+//   }
 
-  return; // never
-}
+//   return; // never
+// }
+
+var missingNumber = function (numbers) {
+  let missingNumber = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    missingNumber += i + 1 - numbers[i];
+  }
+  return missingNumber;
+};
